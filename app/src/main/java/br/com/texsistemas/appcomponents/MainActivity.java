14bbf42c57e8ffa7f85afb5e4bdc1ca6a67613ac
davilnv <davilima871@gmail.com>
2024-06-13 08:10:29 -0300

    Checkbox

--- app/src/main/java/br/com/texsistemas/appcomponents/MainActivity.java
+++ app/src/main/java/br/com/texsistemas/appcomponents/MainActivity.java
@@ -2,6 +2,7 @@ package br.com.texsistemas.appcomponents;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     private TextInputEditText editTextName;
     private TextInputEditText editTextEmail;
     private TextView textResult;
+    private CheckBox checkRed, checkBlack, checkWhite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +25,47 @@ public class MainActivity extends AppCompatActivity {
         editTextEmail = findViewById(R.id.editEmail);
         textResult = findViewById(R.id.textResult);
 
+        checkRed = findViewById(R.id.checkRed);
+        checkBlack = findViewById(R.id.checkBlack);
+        checkWhite = findViewById(R.id.checkWhite);
+
+    }
+
+    public void checkBox() {
+
+        boolean resultRed = checkRed.isChecked();
+        boolean resultBlack = checkBlack.isChecked();
+        boolean resultWhite = checkWhite.isChecked();
+
+        String result = "";
+
+        if (resultRed) {
+            result += checkRed.getText().toString();
+        }
+
+        if (resultBlack) {
+            result += checkBlack.getText().toString();
+        }
+
+        if (resultWhite) {
+            result += checkBlack.getText().toString();
+        }
+
+
+        textResult.setText(String.format("Cores selecionadas: %s", result));
+
     }
 
     public void send(View view) {
 
+        checkBox();
+
+        /*
         String name = editTextName.getText() != null ? editTextName.getText().toString() : null;
         String email = editTextEmail.getText() != null ? editTextEmail.getText().toString() : null;
 
         textResult.setText(String.format("Nome: %s - Email: %s", name, email));
+         */
 
     }
 
